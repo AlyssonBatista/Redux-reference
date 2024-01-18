e5e361cdcba92193754a9e5ebfc93f8295783e09
AlyssonBatista <alyssonbatista241@gmail.com>
2024-01-18 12:39:05 -0300

    add store and reducers

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+```git clone git@github.com:AlyssonBatista/Redux-reference.git```
+
 # Redux-reference
 ````Store````
 > A store da acesso ao estado global da aplicação e ela recebe como argumento o reducer que retorna o estado atual da store.
--- redux/reduxTeste.js
+++ redux/reduxTeste.js
@@ -63,46 +63,119 @@
 
 
 // ================================================================================================================
+// Usando o Redux (pode usar Immer ou Não).
+// Crie uma store contendo os estados iniciais abaixo
+// Crie as seguintes ações:
+// aluno/INCREMENTAR_TEMPO, adiciona 1 dia de acesso
+// aluno/REDUZIR_TEMPO, reduz 1 dia de acesso
+// aluno/MODIFICAR_EMAIL(email), modifica o email do usuário
+// aulas/COMPLETAR_AULA(id), completa a aula com base no ID passado
+// aulas/COMPLETAR_CURSO, completa todas as aulas
+// aulas/RESETAR_CURSO, reseta todas as aulas completas
+// Crie constantes e action creators para as ações.
+// Crie um reducer para aluno e um para aulas.
+// Renderize na tela o nome, email, tempo restante e o total de aulas completas
+// Configure a DevTools
 
 const aluno = {
-    nome: 'André Rafael',
-    email: 'andre@origamid.com',
-    diasRestantes: 120,
-  };
-  
-const aulas = [
-    {
-      id: 1,
-      nome: 'Design',
-      completa: true,
-    },
-    {
-      id: 2,
-      nome: 'HTML',
-      completa: false,
-    },
-    {
-      id: 3,
-      nome: 'CSS',
-      completa: false,
-    },
-    {
-      id: 4,
-      nome: 'JavaScript',
-      completa: false,
-    },
-  ];
+  nome: 'André Rafael',
+  email: 'andre@origamid.com',
+  diasRestantes: 120,
+};
 
+const aulas = [
+  {
+    id: 1,
+    nome: 'Design',
+    completa: true,
+  },
+  {
+    id: 2,
+    nome: 'HTML',
+    completa: false,
+  },
+  {
+    id: 3,
+    nome: 'CSS',
+    completa: false,
+  },
+  {
+    id: 4,
+    nome: 'JavaScript',
+    completa: false,
+  },
+];
+
+// constantes ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+const INCREMENTAR_TEMPO = 'INCREMENTAR_TEMPO';
+const REDUZIR_TEMPO = 'REDUZIR_TEMPO';
+const MODIFICAR_EMAIL = 'MODIFICAR_EMAIL';
+const COMPLETAR_AULA = 'COMPLETAR_AULA';
+const COMPLETAR_CURSO = 'COMPLETAR_CURSO';
+const RESETAR_CURSO =  'RESETAR_CURSO'
+
+
+function incrementar(){
+  return {type: INCREMENTAR_TEMPO }
+}
+
+function reduzirTempo(){
+  return {type: REDUZIR_TEMPO}
+}
+
+function modificaEmail(payload){
+  return {type: MODIFICAR_EMAIL,payload}
+}
+
+function completaAula(payload){
+  return {type: COMPLETAR_AULA,payload}
+}
+
+function completaCurso(){
+  return {type: COMPLETAR_CURSO}
+}
+
+function resetaCurso(){
+  return {type: RESETAR_CURSO}
+}
 
 
 const students = immer.produce((state,action) => {
-
+  switch (action.type){
+    case INCREMENTAR_TEMPO:
+      state.students.diasRestantes + 1;
+      break;
+    case REDUZIR_TEMPO:
+      state.students.diasRestantes - 1;
+      break;
+    case MODIFICAR_EMAIL:
+      state.students.email = action.payload;
+      break;
+  }
 },aluno)
 
 const classes = immer.produce((state,action) => {
-
+  switch (action.type){
+    case COMPLETAR_AULA:
+      state.classes[action.payload].completa = true;
+      break;
+    // case COMPLETAR_CURSO:
+    //   state.classes.forEach(element => {
+    //     state.classes[element].completa = true;
+    //   }
+    // case RESETAR_CURSO:
+    //   state.classes.forEach(element => {
+    //     state.classes[element].completa = false
+    //   })
+  }
 },aulas)
 
 const reducer = Redux.combineReducers({students,classes})
 const store =  Redux.createStore(reducer,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
+store.dispatch(reduzirTempo())
+const state = store.getState()
+console.log(state)
+
+// console.log(store.getState().students.diasRestantes)
